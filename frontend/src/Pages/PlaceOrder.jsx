e4b9a3ab94686953b9f2a89c1b68a2635cb951ce
oofeyang22 <olaitanferanmi41@gmail.com>
2026-07-15 15:44:12 +0100

    changed cursor

--- frontend/src/Pages/PlaceOrder.jsx
+++ frontend/src/Pages/PlaceOrder.jsx
@@ -252,7 +252,7 @@ const PlaceOrder = () => {
         <div className='w-full text-end mt-8'>
           <button
             type='submit'
-            className='bg-black text-white px-16 py-3 text-sm'
+            className='bg-black text-white px-16 py-3 text-sm cursor-pointer'
           >
             PLACE ORDER
           </button>
